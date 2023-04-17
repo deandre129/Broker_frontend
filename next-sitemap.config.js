17c4fc9f088081f398a32fdbb6ea12c8419489b3
markell58 <markell.yager58@gmail.com>
2023-04-17 09:27:26 +0900

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const config = {
+    siteUrl: 'https://broker-bewertungen.de',
+    generateRobotsTxt: true, // (Optional parameter for creating robots.txt file)
+    // Other available options..
+  }
+ 
+module.exports = config;
