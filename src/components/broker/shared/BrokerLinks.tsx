04a0b58d34d43ff3955cc2612c22994ed1391302
thomsjohn <thomsjohnson96@gmail.com>
2024-07-25 10:20:16 -0400

    feat: update broker link in forex-cfd-broker-verglich

--- src/components/broker/shared/BrokerLinks.tsx
+++ src/components/broker/shared/BrokerLinks.tsx
@@ -1,15 +1,15 @@
-import i18n from '@/i18n';
-import MDBox from '@/mui/components/MDBox';
-import MDButton from '@/mui/components/MDButton';
-import MonitorIcon from '@mui/icons-material/Monitor';
-import PropTypes from 'prop-types';
-import SendIcon from '@mui/icons-material/Send';
-import MDTypography from '@/mui/components/MDTypography';
+import i18n from "@/i18n";
+import MDBox from "@/mui/components/MDBox";
+import MDButton from "@/mui/components/MDButton";
+import MonitorIcon from "@mui/icons-material/Monitor";
+import PropTypes from "prop-types";
+import SendIcon from "@mui/icons-material/Send";
+import MDTypography from "@/mui/components/MDTypography";
 
 function BrokerLinks({ record }) {
   return (
     <MDBox display="flex" flexDirection="column" gap={2}>
-      <MDButton
+      {/* <MDButton
         variant="contained"
         href={record.meta?.demo_url}
         color="success"
@@ -25,13 +25,23 @@ function BrokerLinks({ record }) {
         variant="contained"
         href={record.meta?.homepage}
         color="warning"
-        startIcon={<SendIcon style={{fill: '#ffffff'}}/>}
+        startIcon={<SendIcon style={{ fill: "#ffffff" }} />}
         target="_blank"
         fullWidth
       >
-        <div className='white-color'>
-          ZUM BROKER
-        </div> 
+        <div className="white-color">ZUM BROKER</div>
+      </MDButton> */}
+      <MDButton
+        variant="contained"
+        target="_blank"
+        href={record.meta?.homepage}
+        color="warning"
+        startIcon={<SendIcon style={{ fill: "#ffffff" }} />}
+        fullWidth
+      >
+        <div className="white-color">
+          {i18n.entities.broker.text.nowTo(record.name).toUpperCase()}
+        </div>
       </MDButton>
       <style jsx>{`
         .white-color {
